d96a2c6f6e0c48973630306acdc28f35d22703a7
Ayush277 <prince908ayush@gmail.com>
2026-04-24 18:19:07 +0530

    CORS

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const PORT = 3000;
 // Middleware
 app.use(express.json());
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
 // Validation function for node strings
 function validateNodeStrings(dataArray) {
   const validEdges = [];
